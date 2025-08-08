0cab8ab6a85f9df60366e9a1e7acca19392dddd1
wesker84 <57398168+wesker84@users.noreply.github.com>
2025-08-08 10:15:37 +0200

    Update __init__.py
    Signed-off-by: wesker84 <57398168+wesker84@users.noreply.github.com>

--- custom_components/robovacl60/vacuums/__init__.py
+++ custom_components/robovacl60/vacuums/__init__.py
@@ -1,9 +1,9 @@
 from typing import Dict, Type
-from .T2267 import T2267
+from .T2277 import T2277
 from .base import RobovacModelDetails
 
 
 ROBOVAC_MODELS: Dict[str, Type[RobovacModelDetails]] = {
-    "T2267": T2267,
+    "T2277": T2277,
 }
 
